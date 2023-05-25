7d1b2b35dc9cbfefc2e2142a95599ba6baf5002e
Naveen123a <86996465+Naveen123a@users.noreply.github.com>
2023-05-25 08:05:50 +0530

    Add files via upload

---
+++ GetPost/src/main/java/getServlet.java
@@ -0,0 +1,43 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class getServlet
+ */
+public class getServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public getServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String message = request.getParameter("message");
+
+        // Handle the message or perform any desired logic here
+
+        // Send response
+        response.setContentType("text/plain");
+        response.getWriter().write("GET Request Received. Message: " + message);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		        
+		    }
+		}
